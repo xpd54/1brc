717483f1ce29c99ffd7db5a43d6a907c05235d62
Ravi Prakash <raviprakash.xpd54@gmail.com>
2024-12-02 07:41:49 +0800

    Add assert to check all city name is valid

--- input/validate_cities_format.cpp
+++ input/validate_cities_format.cpp
@@ -1,3 +1,22 @@
+#include <cassert>
+#include <fstream>
 #include <iostream>
-
-int main(int argc, char *argv[]) { return 0; }
+#include <string>
+#include <vector>
+int main(int argc, char *argv[]) {
+  std::ifstream file_stream("./measurement_cities.txt");
+  if (!file_stream) {
+    std::cerr << "Can't read cities list." << '\n';
+    return 1;
+  }
+  std::vector<std::string> city_list;
+  std::string city;
+  while (std::getline(file_stream, city, '\n')) {
+    city_list.push_back(city);
+  }
+  for (auto &value : city_list) {
+    assert(value.size() <= 100 && !value.empty());
+  }
+  std::cout << city_list.size() << '\n';
+  return 0;
+}
