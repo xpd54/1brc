9a615dbf3134e75ed45b64907d0e31c284b95848
Ravi Prakash <raviprakash.xpd54@gmail.com>
2024-12-02 14:17:46 +0800

    Generate input file which takes a number as an argument and generate output with random city and random temp; A sample with 100000 entry generated.

--- input/main.cpp
+++ input/main.cpp
@@ -6,7 +6,10 @@
 #include <vector>
 
 #define CITY_LIST "measurement_cities.txt"
+#define INPUT_LIST "measurement.txt"
 
+/*Read the city list which only have single column with city name and push it to
+ * a vector*/
 std::vector<std::string> get_list_of_cities(std::string file_name) {
   std::ifstream file_stream(file_name);
   if (!file_stream) {
@@ -23,21 +26,30 @@ std::vector<std::string> get_list_of_cities(std::string file_name) {
 int main(int argc, char **argv) {
   std::random_device rand_dev;
   std::mt19937 generator(rand_dev());
+
+  /*Create real distribution for city temparature range. City temparature would
+   * vary from -99.9 to 99.9.*/
   std::uniform_real_distribution<double> real_dist(-99.9, 99.9);
+
+  /*Create integral distribution for index of city to randomly pick on from city
+   * list. We have 10000 unique city.*/
   std::uniform_int_distribution<int> integral_dis(0, 10000);
 
   std::vector<std::string> city_list = get_list_of_cities(CITY_LIST);
 
+  /*How many entry to create.*/
   char *entry = argv[1];
   int number_of_entry = atoi(entry);
 
+  /*Pick a random city and assign a random temparature.*/
   std::vector<double> input;
   input.reserve(number_of_entry);
-  for (int i = 0; i < number_of_entry; ++i) {
-    input.push_back(std::round(real_dist(generator) * 10) / 10);
-  }
 
-  std::cout << input.size() << '\n';
-  std::cout << input.back() << '\n';
+  std::ofstream output_file(INPUT_LIST);
+  for (int i = 0; i < number_of_entry; ++i) {
+    output_file << city_list[integral_dis(generator)] << ';'
+                << std::round(real_dist(generator) * 10) / 10 << '\n';
+  }
+  output_file.close();
   return 0;
 }
