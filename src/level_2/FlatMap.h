64c87ebe56ed3af7870270ff5d6ef13dadc09c5c
Ravi Prakash <raviprakash.xpd54@gmail.com>
2024-12-10 04:44:49 +0800

    Update with template of FlatMap

--- src/level_2/FlatMap.h
+++ src/level_2/FlatMap.h
@@ -1,3 +1,19 @@
 #pragma once
 /*Flat map which uses Array and vector to keep track of keys and value rather than Tree. */
-class FlatMap {};
+#include <array>
+#include <cstddef>
+#include <cstdint>
+#include <vector>
+template <typename Key, typename T> class FlatMap {
+  FlatMap();
+  T *find(Key key);
+  std::pair<Key *, bool> emplace(Key key, const T &value);
+  size_t size();
+  T &operator[](const Key &key);
+  std::vector<Key> &sorted_keys();
+
+private:
+  std::array<Key, UINT16_MAX> _keys;
+  std::array<T, UINT16_MAX> _values;
+  std::vector<size_t> filled_indexes;
+};
