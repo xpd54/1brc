842aca0bbcfdd1493ff6efef44efc148e78c40f1
Ravi Prakash <raviprakash.xpd54@gmail.com>
2024-12-10 09:34:30 +0800

    Remove Idea of template flatmap and keep tied up with string::view

---
+++ src/level_2/FlatMap.cpp
@@ -0,0 +1,25 @@
+#include "FlatMap.h"
+#include "../Station.h"
+#include <cstdint>
+#include <string_view>
+FlatMap::FlatMap() : _keys{}, _values{}, filled_indexes{} {}
+
+std::pair<std::string_view *, bool> FlatMap::emplace(std::string_view key, const Station_INT &value) {}
+
+Station_INT &FlatMap::operator[](const std::string_view &key) {}
+
+Station_INT *FlatMap::find(const std::string_view &key) {
+  uint16_t index = get_hash(key);
+  while (!_keys[index].empty()) {
+    if (_keys[index] == key)
+      break;
+    ++index;
+  }
+  return &_values[index];
+}
+
+std::vector<std::string_view> &FlatMap::sorted_keys() {}
+
+size_t FlatMap::size() const { return filled_indexes.size(); }
+
+void FlatMap::reserve(size_t size) { filled_indexes.reserve(size); }
--- src/level_2/FlatMap.h
+++ src/level_2/FlatMap.h
@@ -1,21 +1,26 @@
 #pragma once
 /*Flat map which uses Array and vector to keep track of keys and value rather than Tree. */
+#include "../Station.h"
 #include <array>
-#include <climits>
 #include <cstddef>
 #include <cstdint>
 #include <vector>
-template <typename Key, typename T> class FlatMap {
-  FlatMap() : _keys{}, _values{}, filled_indexes{} {}
-  T *find(Key key);
-  std::pair<Key *, bool> emplace(Key key, const T &value);
-  size_t size();
-  T &operator[](const Key &key);
-  std::vector<Key> &sorted_keys();
-  bool reserve(size_t size);
+class FlatMap {
+  FlatMap();
+  std::pair<std::string_view *, bool> emplace(std::string_view key, const Station_INT &value);
+  Station_INT &operator[](const std::string_view &key);
+  Station_INT *find(const std::string_view &key);
+  std::vector<std::string_view> &sorted_keys();
+  size_t size() const;
+  void reserve(size_t size);
 
 private:
-  std::array<Key, UINT16_MAX + 1> _keys;
-  std::array<T, UINT16_MAX + 1> _values;
+  inline uint16_t get_hash(const std::string_view &key) const {
+    uint16_t index = std::hash<std::string_view>(key);
+    return index;
+  }
+
+  std::array<std::string_view, UINT16_MAX + 1> _keys;
+  std::array<Station_INT, UINT16_MAX + 1> _values;
   std::vector<uint16_t> filled_indexes;
 };
