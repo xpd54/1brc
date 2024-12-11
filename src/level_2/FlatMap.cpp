ae8f1af4bb10d876e14c0dbf4e5e81ad432cbb88
Ravi Prakash <raviprakash.xpd54@gmail.com>
2024-12-11 22:48:47 +0800

    Update flat_map implementation

--- src/level_2/FlatMap.cpp
+++ src/level_2/FlatMap.cpp
@@ -1,24 +1,44 @@
 #include "FlatMap.h"
 #include "../Station.h"
+#include <algorithm>
+#include <cstddef>
 #include <cstdint>
 #include <string_view>
 FlatMap::FlatMap() : _keys{}, _values{}, filled_indexes{}, hash_instance{} {}
 
-std::pair<std::string_view *, bool> FlatMap::emplace(std::string_view key, const Station_INT &value) {}
+std::pair<Station_INT *, bool> FlatMap::emplace(std::string_view key, const Station_INT &value) {
+  // check if there is a key already there if no insert
+  uint16_t index = get_index(key);
+  if (_keys[index].empty()) {
+    filled_indexes.push_back(index);
+    _keys[index] = key;
+    _values[index] = value;
+    return {&_values[index], true};
+  }
 
-Station_INT &FlatMap::operator[](const std::string_view &key) {}
+  // if there is a key return the value at that index
+  return {&_values[index], false};
+}
+
+Station_INT &FlatMap::operator[](const std::string_view &key) {
+  uint16_t index = get_index(key);
+  return _values[index];
+}
 
 Station_INT *FlatMap::find(const std::string_view &key) {
-  uint16_t index = get_hash(key);
-  while (!_keys[index].empty()) {
-    if (_keys[index] == key)
-      break;
-    ++index;
-  }
+  uint16_t index = get_index(key);
   return &_values[index];
 }
 
-std::vector<std::string_view> &FlatMap::sorted_keys() {}
+std::vector<std::string_view> FlatMap::sorted_keys() {
+  auto comp = [&](uint16_t left, uint16_t rigth) { return _keys[left] < _keys[rigth]; };
+  std::sort(filled_indexes.begin(), filled_indexes.end(), comp);
+  std::vector<std::string_view> sorted_keys;
+  for (const auto &index : filled_indexes) {
+    sorted_keys.push_back(_keys[index]);
+  }
+  return sorted_keys;
+}
 
 size_t FlatMap::size() const { return filled_indexes.size(); }
 
--- src/level_2/FlatMap.h
+++ src/level_2/FlatMap.h
@@ -8,19 +8,29 @@
 #include <vector>
 class FlatMap {
   FlatMap();
-  std::pair<std::string_view *, bool> emplace(std::string_view key, const Station_INT &value);
+  std::pair<Station_INT *, bool> emplace(std::string_view key, const Station_INT &value);
   Station_INT &operator[](const std::string_view &key);
   Station_INT *find(const std::string_view &key);
-  std::vector<std::string_view> &sorted_keys();
+  std::vector<std::string_view> sorted_keys();
   size_t size() const;
   void reserve(size_t size);
 
-private:
-  inline uint16_t get_hash(const std::string_view &key) const {
-    uint16_t index = hash_instance(key);
-    return index;
+  inline size_t count(const std::string_view &key) {
+    uint16_t index = get_hash(key);
+    return _keys[index].empty() ? 0 : 1;
   }
 
+private:
+  inline uint16_t get_hash(const std::string_view &key) const { return hash_instance(key); }
+  inline size_t get_index(const std::string_view &key) const {
+    uint16_t index = get_hash(key);
+    while (!_keys[index].empty()) {
+      if (_keys[index] == key)
+        break;
+      ++index;
+    }
+    return index;
+  }
   std::array<std::string_view, UINT16_MAX + 1> _keys;
   std::array<Station_INT, UINT16_MAX + 1> _values;
   std::vector<uint16_t> filled_indexes;
